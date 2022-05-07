f49df1c9cb60bdccb64ade8ed04f800c4c8a810b
Shubham Sharma <shubham.sharma@bluestone.com>
2022-05-07 13:02:12 +0530

    AdMob is integrated

--- app/src/main/java/com/bittslife/newsapp/MainActivity.kt
+++ app/src/main/java/com/bittslife/newsapp/MainActivity.kt
@@ -6,6 +6,12 @@ import android.widget.AbsListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,13 +27,45 @@ class MainActivity : AppCompatActivity() {
     var page = 1
     var totalResult = -1
 
+    private var mInterstitialAd: InterstitialAd? = null
+    var adRequest = AdRequest.Builder().build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this)
+
+        loadAd()
+        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdShowedFullScreenContent() {
+                super.onAdShowedFullScreenContent()
+                loadAd()
+            }
+        }
 
         initViews()
         getNews()
 
+    }
+
+    private fun loadAd() {
+
+        InterstitialAd.load(this,
+            "ca-app-pub-3940256099942544/1033173712",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(p0: InterstitialAd) {
+                    super.onAdLoaded(p0)
+                    mInterstitialAd = p0
+                }
+
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    super.onAdFailedToLoad(p0)
+                    Log.e(TAG, "onAdFailedToLoad: ${p0}}" )
+                }
+
+            })
     }
 
     private fun initViews() {
@@ -54,7 +92,13 @@ class MainActivity : AppCompatActivity() {
                 val itemcount = layoutManager.itemCount
                 val visibleItemsOnScreen = layoutManager.childCount
                 val movedUpItems = layoutManager.findFirstVisibleItemPosition()
-
+                if (movedUpItems % 5 == 0){
+                    if (mInterstitialAd != null) {
+                        mInterstitialAd?.show(this@MainActivity)
+                    } else {
+                        Log.d("TAG", "The interstitial ad wasn't ready yet.")
+                    }
+                }
                 if (totalResult > itemcount && movedUpItems + visibleItemsOnScreen + 5 > itemcount){
                     ++page
                     getNews()
